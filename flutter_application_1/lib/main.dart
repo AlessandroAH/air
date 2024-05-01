9adeb35c37cbff15e812a8373c9098d66838f8e6
AlessandroAH <alessandroaguilar344@gmail.com>
2024-05-01 13:58:55 +0200

    Aggiunta combo box della risposta e caricamento files

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -5,16 +5,23 @@ import 'package:flutter_sound/flutter_sound.dart';
 import 'dart:io';
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:file_picker/file_picker.dart';
 
+//Valori possibili per la lunghezza della risposta
+enum RispostaLunghezza { bassa, media, alta }
+
+//Classe principale dell'applicazione
 void main() {
   runApp(MyApp());
 }
 
+//Classe per la creazione dell'interfaccia grafica
 class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
 }
 
+//Classe per la creazione dell'interfaccia grafica
 class _MyAppState extends State<MyApp> {
   final TextEditingController controller1 = TextEditingController();
   final TextEditingController controller2 = TextEditingController();
@@ -23,7 +30,11 @@ class _MyAppState extends State<MyApp> {
   String? _path;
 
   String dropdownValue = 'Riassumi';
+  RispostaLunghezza _rispostaLunghezza = RispostaLunghezza.bassa;
 
+  //Metodi per la registrazione audio
+
+  //Metodo per aprire la sessione audio
   @override
   void initState() {
     super.initState();
@@ -32,6 +43,7 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
+  //Metodo per chiudere la sessione audio
   @override
   void dispose() {
     _recorder!.closeAudioSession();
@@ -39,39 +51,69 @@ class _MyAppState extends State<MyApp> {
     super.dispose();
   }
 
-Future<void> _record() async {
-  try {
-    if (await Permission.microphone.request().isGranted && await Permission.manageExternalStorage.request().isGranted) {
+  //Metodo per registrare l'audio
+  Future<void> _record() async {
+    try {
+      if (await Permission.microphone.request().isGranted &&
+          await Permission.manageExternalStorage.request().isGranted) {
+        Directory? appDirectory = await getExternalStorageDirectory();
+        if (appDirectory != null) {
+          _path = '${appDirectory.path}/flutter_sound-tmp.wav';
+          print(_path);
+          if (_recorder!.isStopped) {
+            await _recorder!
+                .startRecorder(toFile: _path, codec: Codec.pcm16WAV);
+            ScaffoldMessenger.of(context)
+                .showSnackBar(SnackBar(content: Text('Recording started')));
+          }
+        } else {
+          print('Unable to get the external storage directory');
+        }
+      } else {
+        print('Microphone or storage permission not granted');
+      }
+    } catch (e) {
+      print('Error occurred while recording: $e');
+    }
+  }
+
+//Metodo per fermare la registrazione audio
+  Future<void> _stop() async {
+    try {
+      if (!_recorder!.isStopped) {
+        await _recorder!.stopRecorder();
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text('Recording stopped')));
+      }
+    } catch (e) {
+      print('Error occurred while stopping the recorder: $e');
+    }
+  }
+
+//Metodo per caricare un file
+  Future<void> _uploadFile() async {
+    FilePickerResult? result =
+        await FilePicker.platform.pickFiles(allowMultiple: true);
+
+    if (result != null) {
       Directory? appDirectory = await getExternalStorageDirectory();
       if (appDirectory != null) {
-        _path = '${appDirectory.path}/flutter_sound-tmp.wav';
-        print(_path);
-        if (_recorder!.isStopped) {
-          await _recorder!.startRecorder(toFile: _path, codec: Codec.pcm16WAV);
-          ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Recording started')));
+        for (var platformFile in result.files) {
+          File file = File(platformFile.path!);
+          final String newPath =
+              '${appDirectory.path}/${file.path.split('/').last}';
+          await file.copy(newPath);
+          print('File copied to $newPath');
         }
       } else {
         print('Unable to get the external storage directory');
       }
     } else {
-      print('Microphone or storage permission not granted');
+      print('No file selected');
     }
-  } catch (e) {
-    print('Error occurred while recording: $e');
   }
-}
 
-Future<void> _stop() async {
-  try {
-    if (!_recorder!.isStopped) {
-      await _recorder!.stopRecorder();
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Recording stopped')));
-    }
-  } catch (e) {
-    print('Error occurred while stopping the recorder: $e');
-  }
-}
-
+//Metodo build per la creazione dell'interfaccia grafica
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -120,17 +162,36 @@ Future<void> _stop() async {
                   );
                 }).toList(),
               ),
+              ListTile(
+                title: const Text('Lunghezza Risposta:'),
+                trailing: DropdownButton<RispostaLunghezza>(
+                  value: _rispostaLunghezza,
+                  onChanged: (RispostaLunghezza? newValue) {
+                    setState(() {
+                      _rispostaLunghezza = newValue!;
+                    });
+                  },
+                  items: RispostaLunghezza.values.map((RispostaLunghezza value) {
+                    return DropdownMenuItem<RispostaLunghezza>(
+                      value: value,
+                      child: Text(value.toString().split('.').last),
+                    );
+                  }).toList(),
+                ),
+              ),
               ElevatedButton(
                 child: Text('Invia'),
                 onPressed: () {
-                  apiService.sendData(controller1.text + ","+ controller1.text);
+                  apiService
+                      .sendData(controller1.text + "," + controller1.text + "," + _rispostaLunghezza.toString());
                 },
               ),
               TextField(
                 controller: controller2,
                 decoration: InputDecoration(
-                  hintText: 'Inserisci altri dati',
+                  hintText: 'Risoposta...',
                 ),
+                enabled: false,
               ),
             ],
           ),
@@ -162,6 +223,9 @@ Future<void> _stop() async {
                   await Future.delayed(
                       Duration(seconds: 5)); // Record for 1 second
                   await _stop();
+                  break;
+                case 2:
+                  await _uploadFile();
                   break;
               }
             },
